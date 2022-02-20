ac5d82d9b37c510141a8c601f52a80011ff7a8c7
somnathsardar <githubsomnath1996@gmail.com>
2022-02-20 11:27:38 +0530

    Displaying routes info.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,20 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Display our routes
+     *
+     * @return string
+     */
+    public function routes()
+    {
+        exec(base_path('artisan route:list'), $routes);
+        foreach ($routes as $index => $route) {
+            if (str_contains($route, 'debugbar')) {
+                unset($routes[$index]);
+            }
+        }
+        return '<pre>' . implode("\n", $routes) . '</pre>';
+    }
 }
